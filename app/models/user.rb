92a1dea9f1fc262657b635d2e693f364504d23af
Lior Levy <lior.pmtraining@gmail.com>
2019-08-26 16:15:30 +0200

    added model words and junktable user_words

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :words, through: :user_words
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :username, presence: true, uniqueness: true
+
 end
---
+++ db/migrate/20190826140828_create_words.rb
@@ -0,0 +1,15 @@
+class CreateWords < ActiveRecord::Migration[5.2]
+  def change
+    create_table :words do |t|
+      t.string :entry
+      t.string :translation
+      t.text :definition
+      t.text :example
+      t.string :type
+      t.integer :difficulty
+      t.string :synonyms, array: true
+
+      t.timestamps
+    end
+  end
+end
