4a8700662b3e7a99ddf8338590be152fa08a3800
axelle bourneuf <axellebourneuf@hotmail.fr>
2019-08-26 17:25:20 +0200

    authorization & pundit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,52 @@
 class ListsController < ApplicationController
+  authorize @list
+
+  def index
+    @lists = policy_scope(List).order(created_at: :desc)
+  end
+
+  def new
+    @list = current_user.lists.new
+    authorize @list
+  end
+
+  def edit
+  end
+
+  def create
+    @list = current_user.lists.new(list_params)
+    authorize @list
+
+    if @list.save
+      redirect_to @list
+    else
+      render :new
+    end
+  end
+
+  def update
+    @list.update(list_params)
+    if @list.save
+
+      redirect_to @list
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :description, :user_id)
+  end
+
+  def set_list
+    @list = list.find(params[:id])
+    authorize @list
+  end
 end
