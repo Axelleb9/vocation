140eb6e14ba2ea6d17dc412f4cf08fb80c8d376c
Lior Levy <lior.pmtraining@gmail.com>
2019-08-27 22:41:05 +0200

    refacto words index + added cross to close translation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,9 @@
 import "bootstrap";
-import { displaySubmit, undisplaySubmit } from '../components/translator-submit-view'
+import { displaySubmit, undisplaySubmit, closeWithCross } from '../components/translator-submit-view'
 // import { translate } from '../components/translate-word';
 
 // translate()
 
-displaySubmit()
-undisplaySubmit()
+displaySubmit();
+undisplaySubmit();
+closeWithCross();
