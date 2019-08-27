82f54463977779e7fd591a862b17f9d71c11a3a1
Raphael Antonietti <antonietti.raphael@gmail.com>
2019-08-27 22:39:34 +0200

    adding animation to chevron

---
+++ app/javascript/components/word-card-display-view.js
@@ -0,0 +1,10 @@
+// const displayContent = document.querySelector(".display-content");
+// const chevronIcon = document.getElementById("chevron-icon");
+
+// const rotateMe = () => {
+//   displayContent.addEventListener("focus", (event) => {
+//     chevronIcon.classList.add("rotate-me");
+//   });
+// }
+
+// export { rotateMe }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,11 @@
 import "bootstrap";
 import { displaySubmit, undisplaySubmit } from '../components/translator-submit-view'
+import { rotateMe }  from '../components/word-card-display'
+
 // import { translate } from '../components/translate-word';
 
 // translate()
 
 displaySubmit()
 undisplaySubmit()
+rotateMe()
