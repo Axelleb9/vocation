356268215dc43c11faa3621f5122eea47f2e6d83
Raphael Antonietti <antonietti.raphael@gmail.com>
2019-08-29 10:57:18 +0200

    added flashcard to lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_145630) do
+ActiveRecord::Schema.define(version: 2019_08_29_085521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_08_28_145630) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "order", default: true
+    t.string "flashcard", array: true
     t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
