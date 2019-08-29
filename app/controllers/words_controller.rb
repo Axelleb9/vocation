751a5f02e63240f6c2e430dae0cb6fad7bbed59d
Lior Levy <lior.pmtraining@gmail.com>
2019-08-29 14:08:01 +0200

    translation rectified

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,5 @@
 class WordsController < ApplicationController
-    skip_after_action :verify_authorized, only: [:change_order, :create]
+  skip_after_action :verify_authorized, only: [:change_order, :create]
 
   def index
     policy_scope(Word)
@@ -12,16 +12,14 @@ class WordsController < ApplicationController
   def create
     word = Word.find_by(entry: params_word[:entry])
     if word.nil?
-      translation = translate_word(params_word[:entry])
-      word = Word.new(
-        entry: params_word[:entry],
-        translation: translation
-      )
+      entry = params_word[:entry]
+      word = Word.new(entry: entry)
       word.save
-      FetchExampleJob.perform_later(translation, current_user.id, word.id)
-      FetchDifficultyJob.perform_later(translation, current_user.id, word.id)
-      FetchDefinitionJob.perform_later(translation, current_user.id, word.id)
-      FetchSynonymsJob.perform_later(translation, current_user.id, word.id)
+      FetchTranslationJob.perform_later(entry, current_user.id, word.id)
+      FetchExampleJob.perform_later(entry, current_user.id, word.id)
+      FetchDifficultyJob.perform_later(entry, current_user.id, word.id)
+      FetchDefinitionJob.perform_later(entry, current_user.id, word.id)
+      FetchSynonymsJob.perform_later(entry, current_user.id, word.id)
     end
     redirect_to words_path(word_id: word.id)
   end
@@ -50,13 +48,5 @@ class WordsController < ApplicationController
 
   def params_word
     params.require(:word).permit(:entry)
-  end
-  def translate_word(word)
-    base = "https://translate.yandex.net/api/v1.5/tr.json/translate?"
-    key = ENV["YANKEY"]
-    languages = "fr-en"
-    url = "#{base}lang=#{languages}&key=#{key}&text=#{word}"
-    callback = JSON.parse(HTTP.get(url))
-    callback["text"].first
   end
 end
