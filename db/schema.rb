a7852bdcd08240551544406736a14597290a3a8d
Lior Levy <lior.pmtraining@gmail.com>
2019-09-03 01:33:52 +0200

    un peu de front pour le quizz

--- db/schema.rb
+++ db/schema.rb
@@ -98,11 +98,8 @@ ActiveRecord::Schema.define(version: 2019_09_02_160027) do
   create_table "words", force: :cascade do |t|
     t.string "entry"
     t.string "translation"
-    t.text "definition", array: true
-    t.text "example"
-    t.string "nature", array: true
+    t.text "example", array: true
     t.integer "difficulty"
-    t.string "synonyms", array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "visible", default: false
@@ -115,7 +112,7 @@ ActiveRecord::Schema.define(version: 2019_09_02_160027) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "reviewed", default: false
-    t.boolean "quizz_status", default: false
+    t.boolean "quizz_status"
     t.index ["list_id"], name: "index_words_lists_on_list_id"
     t.index ["word_id"], name: "index_words_lists_on_word_id"
   end
