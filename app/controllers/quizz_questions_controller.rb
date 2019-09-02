ea13d7cfbe8b0be869b4c6bffe974b92db4f5dcc
Lior Levy <lior.pmtraining@gmail.com>
2019-09-02 21:12:30 +0200

    structure des fichiers

--- app/controllers/quizz_questions_controller.rb
+++ app/controllers/quizz_questions_controller.rb
@@ -1,17 +1,21 @@
 class QuizzQuestionsController < ApplicationController
-
+  after_action :skip_authorization
   def question
-    list = List.find(params[:list_id])
-    word = list.words_lists.where(quizz_status: false).sample
-    word.update(quizz_status: true)
+    @list = List.find(params[:list_id])
+    @word = @list.words_lists.where(quizz_status: false).sample
+    @word.update(quizz_status: true)
     luck = (1..5).to_a.sample
     case luck
     when 1
+      @question = 1
     when 2
+      @question = 2
     when 3
+      @question = 3
     when 4
+      @question = 4
     when 5
+      @question = 5
     end
   end
-
 end
