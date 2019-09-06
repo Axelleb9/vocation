025fac433f7df071edf2183ae99f3be08feba0f5
Raphael Antonietti <antonietti.raphael@gmail.com>
2019-09-06 11:28:33 +0200

    done with flashcards

--- app/javascript/components/flashcard-switch.js
+++ app/javascript/components/flashcard-switch.js
@@ -1,21 +1,21 @@
-const wordQuestion = document.getElementById("flashcard-question");
-const wordAnswer = document.getElementById("flashcard-answer");
-const flashcardButton = document.getElementById("flashcard-button");
-const goodAnswer = document.getElementById("good-answer");
-const wrongAnswer = document.getElementById("wrong-answer");
-const seeMeaning = document.getElementById("see-meaning");
+// const wordQuestion = document.getElementById("flashcard-question");
+// const wordAnswer = document.getElementById("flashcard-answer");
+// const flashcardButton = document.getElementById("flashcard-button");
+// const goodAnswer = document.getElementById("good-answer");
+// const wrongAnswer = document.getElementById("wrong-answer");
+// const seeMeaning = document.getElementById("see-meaning");
 
 
-const switchCard = () => {
-  if (flashcardButton) {
-    flashcardButton.addEventListener("click", (event) => {
-      wordQuestion.classList.add("d-none");
-      wordAnswer.classList.remove("d-none");
-      wrongAnswer.classList.remove("d-none");
-      goodAnswer.classList.remove("d-none");
-      seeMeaning.classList.add("d-none");
-    });
-  }
-};
+// const switchCard = () => {
+//   if (flashcardButton) {
+//     flashcardButton.addEventListener("click", (event) => {
+//       wordQuestion.classList.add("d-none");
+//       wordAnswer.classList.remove("d-none");
+//       wrongAnswer.classList.remove("d-none");
+//       goodAnswer.classList.remove("d-none");
+//       seeMeaning.classList.add("d-none");
+//     });
+//   }
+// };
 
-export { switchCard };
+// export { switchCard };
